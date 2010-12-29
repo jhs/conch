a16482f3d3a48b8ea5c770887291818add7cdd8b
Jason Smith <jhs@couchone.com>
2010-12-29 16:48:32 +0700

    Don't use knockout.mapping for now, and do use jQuery as a KO dependency

--- couchapp/_attachments/js/require_knockout.js
+++ couchapp/_attachments/js/require_knockout.js
@@ -1,8 +1,10 @@
 // RequireJS wrapper to pull in KnockoutJS
 //
 
-define([ 'order!ddoc/vendor/knockout/knockout-1.1.1.js'
-       , 'order!ddoc/vendor/knockout/knockout.mapping-latest.js'
+define([ 'order!jquery' // Looks like Knockout will use Jquery if it's available.
+       , 'order!ddoc/vendor/knockout/knockout-1.1.1.js'
+       // knockout.mapping is disabled for now. There is a bug, unsure whether mine or theirs.
+       //, 'order!ddoc/vendor/knockout/knockout.mapping-latest.js'
        ], function() {
   // Return the Knockout API as this module.
   return ko;
